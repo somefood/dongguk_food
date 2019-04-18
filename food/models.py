f8619bcc36bc4ed6eb0b99cf4d2186eeb6b35b42
somefood <hsj46655@gmail.com>
2019-04-18 11:26:30 +0900

    first commit

---
+++ food/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+class Store(models.Model):
+    name = models.CharField(max_length=50)
+    location = models.CharField(max_length=100)
+    phone_number = models.CharField(max_length=30, blank=True)
+
+    def __str__(self):
+        return self.name
+
+class Menu(models.Model):
+    store = models.ForeignKey(Store, on_delete=models.CASCADE)
+    menu = models.CharField(max_length=50, default='입력')
+    votes = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.menu
