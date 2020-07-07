e6df8cb66ce8cc9382183f3c551b886cae8e9ba4
somefood <hsj4665@gmail.com>
2020-07-07 13:40:53 +0900

    수정: base.py 수정

--- mysite/settings/base.py
+++ mysite/settings/base.py
@@ -137,11 +137,11 @@ if DEBUG:
 else:
     AUTH_USER_MODEL = 'accounts.User'
     EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-    EMAIL_HOST_USER = ''
+    EMAIL_HOST_USER = os.environ['EMAIL_HOST_USER']
     EMAIL_HOST = 'smtp.gmail.com'
     EMAIL_PORT = 587
     EMAIL_USE_TLS = True
-    EMAIL_HOST_PASSWORD = ""
+    EMAIL_HOST_PASSWORD = os.environ['EMAIL_HOST_PASSWORD']
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
