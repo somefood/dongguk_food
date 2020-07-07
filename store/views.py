ea019467e854b814610ace29d7ad0adc01c9b723
somefood <hsj4665@gmail.com>
2020-07-07 20:42:12 +0900

    수정: 카테고리 버튼 추가, 순위 수정 가게 보기 페이지에서 카테고리 버튼 추가 다음페이지 넘어가도 순위 유지되게 수정

--- store/views.py
+++ store/views.py
@@ -39,9 +39,27 @@ class StoreIndexView(ListView):
 class CategoryView(ListView):
     template_name = 'store/index.html'
     context_object_name = 'store_list'
+    paginate_by = 10
 
     def get_queryset(self):
         return Store.objects.filter(category=self.kwargs['category'])
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        paginator = context['paginator']
+        page_numbers_range = 5  # Display only 5 page numbers
+        max_index = len(paginator.page_range)
+
+        page = self.request.GET.get('page')
+        current_page = int(page) if page else 1
+        start_index = int((current_page - 1) / page_numbers_range) * page_numbers_range
+        end_index = start_index + page_numbers_range
+        if end_index >= max_index:
+            end_index = max_index
+
+        page_range = paginator.page_range[start_index:end_index]
+        context['page_range'] = page_range
+        return context
 
 @login_required
 def like(request):
