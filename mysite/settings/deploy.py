d19dd09795cd34c60a79531152a79d63a8543648
somefood <hsj4665@gmail.com>
2020-07-07 17:15:42 +0900

    수정: deploy 파일에 메일 관련 내용 삽입

--- mysite/settings/deploy.py
+++ mysite/settings/deploy.py
@@ -2,5 +2,13 @@ from .base import *
 
 DEBUG = False
 
+AUTH_USER_MODEL = 'accounts.User'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST_USER = os.environ['EMAIL_HOST_USER']
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_PASSWORD = os.environ['EMAIL_HOST_PASSWORD']
+
 DISQUS_SHORTNAME = 'donnguk-food'
 DISQUS_MY_DOMAIN = 'http://somefood.pythonanywhere.com'
